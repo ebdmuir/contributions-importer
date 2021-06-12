218bf902e0e0874ee31d967072f1da5124c40a8d
Eric Muir <eric@ebdm.dev>
2021-06-12 19:42:30 +0100

    Iterates over list

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import git 
 from git_contributions_importer import *
-# Your private repo or Bitbucket repo
-tfbaseinstance = git.Repo("terraform-aws-base-instance")
-swimcloudbackend = git.Repo("backend-api")
-swimcloudwebapps = git.Repo("web-applications")
-swimcloudfinder = git.Repo("lesson-finder")
-swimcloudportal = git.Repo("customer-portal")
+import json
+
+data = json.load(open('data.json', 'r'))
+repos = []
+
+for item in data:
+	git.Repo.clone_from(item.repo, item.name)
+	repos.append(git.Repo(item.name))
+
 # Your mock repo
 mock_repo = git.Repo("contributions")
-importer = Importer([tfbaseinstance, swimcloudbackend, swimcloudwebapps, swimcloudfinder, swimcloudportal], mock_repo) # I use both my personal email and work email here,
+importer = Importer(repos, mock_repo) # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github profiles uses
 # my work email
 importer.set_author(['eric@ebdm.dev', 'eric.muir@swimtime.org', 'www-data@ip-172-0-253-183.eu-west-2.compute.internal', 'www-data@ip-172-0-46-95.eu-west-2.compute.internal', 'ericmuir@protonmail.com', 'ericmuir@Erics-iMac.local', 'ebdmuir@gmail.com'])
